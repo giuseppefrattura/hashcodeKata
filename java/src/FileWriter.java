477f55de914076a7e6469574b21af911219fc528
Giuseppe Frattura <giuseppefrattura@gmail.com>
2021-02-20 17:44:39 +0100

    first version of file writer

--- java/src/FileWriter.java
+++ java/src/FileWriter.java
@@ -1,25 +1,34 @@
 
 import matteo.beans.Book;
 import matteo.beans.Library;
+import matteo.beans.Result;
 
 import java.io.*;
-import java.io.FileReader;
-import java.util.ArrayList;
 
 
 public class FileWriter {
 
-    public void writeFile1() throws IOException {
+    public void writeFile(Result result) throws IOException {
         File fout = new File("out.txt");
         FileOutputStream fos = new FileOutputStream(fout);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        int totLibraries = result.getAllLibraries().size();
+        bw.write(String.valueOf(totLibraries));
+        bw.newLine();
 
         //Logica
-        for (int i = 0; i < 10; i++) {
-            bw.write("something");
+        Library library;
+        for (int i = 0; i < totLibraries; i++) {
+            library = result.getAllLibraries().get(i);
+            bw.write(String.valueOf(library.getId()) +" " + String.valueOf(library.getBooksList().size()));
+            bw.newLine();
+            String bookIdString= "";
+            for (Book booksList : library.getBooksList()){
+                bookIdString += booksList.getId() + " ";
+            }
+            bw.write(bookIdString);
             bw.newLine();
         }
-
 
         bw.close();
     }
